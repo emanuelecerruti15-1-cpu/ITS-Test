db3950ad48a1e3fafe04e0be7537004e1e999c82
EmanueleCerruti <emanuele.cerruti.15-1@accademiadigitaleliguria.it>
2026-05-25 11:10:13 +0200

    Corretto il file java

--- test.java
+++ test.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 public class boia {
-  public static void main(){
+  public static void main(String args[]) {
     String merda;
-    Scanner tastiera = new Scanner;
+    Scanner tastiera = new Scanner(System.in);
     System.out.println("Inserisci una parola");
     merda = tastiera.nextLine();
     System.out.println("Mannaggia a dio");
-    Sytem.out.println(merda);
+    System.out.println(merda);
   }
 }
