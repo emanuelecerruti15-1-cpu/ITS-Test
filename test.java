046351ceceb609336721a3be6746e9b273b163f5
EmanueleCerruti <emanuele.cerruti.15-1@accademiadigitaleliguria.it>
2026-05-25 11:29:41 +0200

    sfogo
    Added comments with personal remarks in test.java.

--- test.java
+++ test.java
@@ -9,6 +9,9 @@ public class boia {
     System.out.println("Mannaggia a dio");
     System.out.println(merda);
 =======
+  //Shara sei un figlio di negra
+  //Penserò a te mentre mi mangio il mio tacos xl
+  //suca
   public static void main(){
     String crazy;
     Scanner tastiera = new Scanner;
