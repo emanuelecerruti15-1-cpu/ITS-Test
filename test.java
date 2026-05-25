1739df664808414bea01c28a70052f4505215cf2
EmanueleCerruti <emanuele.cerruti.15-1@accademiadigitaleliguria.it>
2026-05-25 10:49:44 +0200

    Add main method to boia class with user input
    Fix syntax errors and improve user input handling.

---
+++ test.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class boia {
+  public static void main(){
+    String merda;
+    Scanner tastiera = new Scanner;
+    System.out.println("Inserisci una parola");
+    merda = tastiera.nextLine();
+    System.out.println("Mannaggia a dio");
+    Sytem.out.println(merda);
+  }
+}
